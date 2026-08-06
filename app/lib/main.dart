0ccfdd58261803d6d94af2bba595dc2ff65df07b
HostWebs <danielsem65@gmail.com>
2026-08-06 05:12:33 +0000

    Import foundation for setEquals

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:floating/floating.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:media_kit/media_kit.dart';
